a84d3b2112bc8070240cf99dfa3ec767e220b19b
Brenda Hernández Rodríguez <hebrenda@ad.corp.google.com>
2021-07-26 02:50:09 -0500

    - Componentes login, home, myprofile, header y footer - Login funcional con firebase auth

--- bren-mk/src/app/pages/login/login.component.ts
+++ bren-mk/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators  } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	constructor(private afAuth: AngularFireAuth, private router: Router, 
+  				private fb: FormBuilder, private ngZone: NgZone) {
 
-  constructor() { }
+	}
 
-  ngOnInit(): void {
-  }
+	loginForm = this.fb.group({
+		email: ['', Validators.required],
+		password: ['', Validators.required]
+	})
 
+ngOnInit() {
+	this.afAuth.user.subscribe(user => {
+		if (user) {
+		this.ngZone.run(() => {
+			this.router.navigate(['/myprofile']);
+    	})
+	})
+
+	signIn() {
+		this.afAuth.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password).then(() => {
+			this.router.navigate(['/myprofile']);
+		}).catch(response => {
+			this.errorMessage = response.message;
+	});
 }
